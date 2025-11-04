c26ef424ab82be66a2895d89bf5e28e22e958811
Matthew Eastwood <ilovelego@outlook.com>
2025-11-03 20:54:46 -0500

    created partial video game model

---
+++ Media-Manager.Core/Models/VideoGame.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MediaManager.Core.Models;
+
+public class VideoGame
+{
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The game must have a title")]
+    [MaxLength(100, ErrorMessage = "The max length of the title is 100 characters")]
+    public string Title { get; set; } = string.Empty;
+    [MaxLength(500, ErrorMessage = "The max length of the description is 500 characters")]
+    public string Description { get; set; } = string.Empty;
+    [Range(0, int.MaxValue, ErrorMessage = "The user's play time must be at least 0")]
+    public int UserPlayTime { get; set; } = 0;
+    [Range(0, int.MaxValue, ErrorMessage = "The estimated play time must be at least 0")]
+    public int EstimatedPlayTime { get; set; } = 0;
+    // public ICollection<VideoGameTagEnum> Tags { get; set; }
+    // public Review GameReview { get; set; }
+    // public ICollection<DailyLog> DailyLogs { get; set; }
+
+}
