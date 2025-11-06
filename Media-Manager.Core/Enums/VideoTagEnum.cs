f703fc39f52650616e6b6ec3163c3a3df1b797b8
Daniel Kandiah <kandiahd@sheridancollege.ca>
2025-11-05 21:08:00 -0500

    video tag enum

---
+++ Media-Manager.Core/Enums/VideoTagEnum.cs
@@ -0,0 +1,9 @@
+namespace MediaManager.Core.Enums;
+public enum VideoTagEnum
+{
+    Action, Adventure, Animation, Anime, Biography, Comedy, Crime,
+    Documentary, Drama, Family, Fantasy, Film_Noir, History, Horror,
+    Music, Musical, Mystery, News, Reality, Romance, Science_Fiction,
+    Sports, Superhero, Talk_Show, Thriller, War, Western, Game_Show, Educational
+
+}
