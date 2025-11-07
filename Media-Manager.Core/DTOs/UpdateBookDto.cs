a83f5be2aa9dbe9b3b9550ed0655dd145c868b46
elibase <eli@base>
2025-11-07 12:24:50 -0500

    created Book Dtos for create & update

---
+++ Media-Manager.Core/DTOs/UpdateBookDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Media_Manager.Core.DTOs;
+
+public class UpdateBookDto
+{
+    [Required(ErrorMessage = "The Book Must have an author")]
+    [MaxLength(100, ErrorMessage = "Character amount exceeds the maximum amount")]
+    public string AuthorName { get; set; } = string.Empty;
+    [Required]
+    [MaxLength(100, ErrorMessage = "Character amount exceeds the maximum amount")]
+    public string Title { get; set; } = string.Empty;
+    [Required]
+    [MaxLength(250, ErrorMessage = "Character amount exceeds the maximum amount")]
+    public string Summary { get; set; } = string.Empty;
+    [Required]
+    public string Genre { get; set; } = string.Empty;
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "The Book Must have a minimum of atleast 1 page")]
+    public int NumberOfPages { get; set; }
+    [Required]
+    public int PublicationYear { get; set; }
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
