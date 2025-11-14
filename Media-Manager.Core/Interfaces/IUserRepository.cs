4250ddf4dc6f9b4fe2f8f129fc2dca2e1d8c2770
Matthew Eastwood <ilovelego@outlook.com>
2025-11-14 10:30:00 -0500

    add methods to repo interface for media object related gets

--- Media-Manager.Core/Interfaces/IUserRepository.cs
+++ Media-Manager.Core/Interfaces/IUserRepository.cs
@@ -4,8 +4,12 @@ namespace MediaManager.Core.Interfaces;
 
 public interface IUserRepository
 {
-    Task<ICollection<User>> GetallAsync();
+    Task<ICollection<User>> GetAllAsync();
     Task<User> GetByIdAsync(int id);
+    Task<ICollection<MediaObject>> GetAllMediaObjectsAsync();
+    Task<ICollection<VideoGame>> GetAllVideoGamesAsync();
+    Task<ICollection<Video>> GetAllVideosAsync();
+    Task<ICollection<Book>> GetAllBooksAsync();
     Task<User> CreateAsync(User user, Role role);
     Task<User> UpdateAsync(User? user, Role? role);
     Task<bool> DeleteAsync(int id);
