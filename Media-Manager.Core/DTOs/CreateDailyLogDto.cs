0bf9d8feea5b5ecc169bc8b34c5fbfbb210da81f
Matthew Eastwood <ilovelego@outlook.com>
2025-11-27 11:44:17 -0500

    create daily log dto

---
+++ Media-Manager.Core/DTOs/CreateDailyLogDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MediaManager.Core.DTOs;
+
+public class CreateDailyLogDto
+{
+    [Required]
+    [MaxLength(500)]
+    public string Content {get;set;} = string.Empty;
+    // Usage Time will be measured in hours. This will be how much the user has used the media object that day. 
+    [Required]
+    public double UsageTime {get;set;}
+}
