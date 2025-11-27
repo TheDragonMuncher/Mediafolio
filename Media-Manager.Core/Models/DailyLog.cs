8dcd3a3d09957b2cf5ce876bec87ed7d7d9f6a7e
Matthew Eastwood <ilovelego@outlook.com>
2025-11-27 11:25:14 -0500

    Create daily log model

---
+++ Media-Manager.Core/Models/DailyLog.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MediaManager.Core.Models;
+
+public class DailyLog
+{
+    public int Id {get;set;}
+    [Required]
+    [MaxLength(500)]
+    public string Content {get;set;} = string.Empty;
+    // Usage Time will be measured in hours. This will be how much the user has used the media object that day. 
+    [Required]
+    public double UsageTime {get;set;}
+    public DateTime CreatedAt {get;set;}
+    public MediaObject? MediaObject {get;set;}
+    public int MediaObjectId {get;set;}
+}
