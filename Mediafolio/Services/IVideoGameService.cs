ffb06e693971aedef525e9c4b251dfa4199f578a
Matthew Eastwood <eastwmat@sheridancollege.ca>
2025-12-02 11:37:49 -0500

    create video game service interface

---
+++ Mediafolio/Services/IVideoGameService.cs
@@ -0,0 +1,9 @@
+namespace Mediafolio.Services;
+
+public interface IVideoGameService {
+    Task<ICollection<VideoGame>> GetAllGames();
+    Task<VideoGame?> GetGameById(int id);
+    Task<VideoGame> CreateGame(CreateVideoGameDto dto);
+    Task<VideoGame> UpdateGame(UpdateVideoGameDto dto);
+    Task<bool> DeleteGame(int id);
+}
