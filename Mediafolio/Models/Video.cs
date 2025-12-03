15c50991a9ab8890dcb067ca78ac835d41ccf47f
Danjkah <daniel02_02@outlook.com>
2025-12-03 01:10:07 -0500

    made video models, review models and put tags for front end

---
+++ Mediafolio/Models/Video.cs
@@ -0,0 +1,36 @@
+using System.Text.Json.Serialization;
+using Mediafolio.Enums;
+namespace Mediafolio.Models;
+
+    public class Video
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; } 
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; } 
+
+    [JsonPropertyName("userWatchTime")]
+    public double UserWatchTime { get; set; }
+
+    [JsonPropertyName("videoDuration")]
+    public int VideoDuration { get; set; }
+
+    [JsonPropertyName("numberOfEpisodes")]
+    public int NumberOfEpisodes { get; set; }
+
+    [JsonPropertyName("tags")]
+    public ICollection<VideoTagEnum>? Tags { get; set; }
+
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+
+    [JsonPropertyName("mediaObjectId")]
+    public int MediaObjectId { get; set; }
+}
